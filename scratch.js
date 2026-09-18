bea4f06e2f5ac0c7bf493947b1c5bfd71f672b1c
ilhamdesnarum <ilham.desnarum@gmail.com>
2026-09-18 09:11:47 +0700

    security: migrate credentials to GitHub secrets and restore hourly sync

--- scratch.js
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); const content = fs.readFileSync('src/App.jsx', 'utf8'); const lines = content.split(/\r?\n/); lines.forEach((line, index) => { if (line.toLowerCase().includes('statusoptions')) console.log('Line ' + (index + 1) + ': ' + line); });
